f8fe02dfece041e5d51e0c7415822bb677fad3e3
geopwebster <51906288+geopwebster@users.noreply.github.com>
2020-09-10 12:05:03 +0800

    finished shopping basket

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,11 @@ export const initialState = {
   basket: [],
 };
 
+// Selector
+export const getBasketTotal = basket =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+// maps through the basket & tally the total
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
@@ -10,6 +15,29 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        basketItem => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product(id: ${action.id}) as its not in the basket!`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
+    // return{
+    //   ...state,
+    //   basket: state.basket.filter(item=>item.id !==action.id)
+    // } //THIS DOES NOT WORK. IT WILL REMOVE ALL ITEMS WITH THE SAME ID (MULTIPLE ITEMS)
+
     default:
       return state;
   }
